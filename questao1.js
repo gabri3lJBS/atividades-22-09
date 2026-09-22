2c98866791be820df2edd03b9108c4a651d40438
gabri3lJBS <gabrielsantana220410@gmail.com>
2026-09-22 16:34:05 -0300

    Add files via upload

---
+++ questao1.js
@@ -0,0 +1,8 @@
+function calcularAreaRetangulo(){
+    let base =Number(prompt("digite o valor da base do retangulo"))
+    let altura =Number(prompt("digite o valor da altura do retangulo"))
+    let area = base*altura
+    return area
+}
+
+alert(calcularAreaRetangulo())
