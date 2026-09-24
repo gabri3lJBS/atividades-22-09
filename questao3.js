6ba92e74f603e12a6f70b7608eeed25910c221da
gabri3lJBS <gabrielsantana220410@gmail.com>
2026-09-24 14:27:33 -0300

    Add files via upload

---
+++ questao3.js
@@ -0,0 +1,6 @@
+function celciusParaFahrenhit(){
+    let  celcius = Number(prompt("Digite a temperatura em celcius"))
+    let fahrenhit = (celcius * 1.8) + 32
+    return fahrenhit
+}
+alert(celciusParaFahrenhit())
